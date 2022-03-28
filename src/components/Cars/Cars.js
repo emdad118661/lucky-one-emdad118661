22593549899c9b3f9183263f05bf4b01a1b0dcff
emdad118661 <emdad118661@gmail.com>
2022-03-28 23:06:16 +0600

    car array making

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './Cars.css';
 const Cars = () => {
+    const cars = [
+        { id: 1, Name: 'Toyota', price: 20000000 },
+        { id: 2, Name: 'Hyndai', price: 40975325 },
+        { id: 3, Name: 'Farrari', price: 40975325 },
+        { id: 4, Name: 'Mahidra', price: 40975325 },
+        { id: 5, Name: 'Tata', price: 40975325 },
+        { id: 6, Name: 'BMW', price: 40975325 },
+        { id: 7, Name: 'Marsetis', price: 40975325 },
+        { id: 8, Name: 'Ford', price: 40975325 },
+        { id: 9, Name: 'Sujuki', price: 40975325 },
+    ];
     return (
-        <div>
-            <div className="cars-containers">
+        <div className='cars-container'>
+            <div className="cards-containers">
                 <h3>this is for cars</h3>
             </div>
             <div className="choose-container">
