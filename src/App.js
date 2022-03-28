5ef54b33b5275e45e64c1c718f8cd2f88e2f32b7
emdad118661 <emdad118661@gmail.com>
2022-03-28 22:36:47 +0600

    cars component folder creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Cars from './components/Cars/Cars';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Cars></Cars>
     </div>
   );
 }
---
+++ src/components/Cars/Cars.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Cars = () => {
+    return (
+        <div>
+            <div className="cars-containers">
+                <h3>this is for cars</h3>
+            </div>
+            <div className="choose-container">
+                <h3>choose randomly</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Cars; <h2>This is list of cars</h2>
